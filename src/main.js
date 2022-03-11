e0e00b4ca069940101ac5c464276c74b3490823e
Théo Roland <theo@monolith.studio>
2022-03-11 14:00:31 +0100

    Update with Splide

--- src/main.js
+++ src/main.js
@@ -1,8 +1,150 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
+import $ from 'jquery'
+$(document).ready(function () {
+  // GLOBAL | Calcul des hauteurs d'écrans
+  const TwoWindows = window.innerHeight * 2
+  const ThreeWindows = window.innerHeight * 3
+  const FourWindows = window.innerHeight * 4
+  const FiveWindows = window.innerHeight * 5
+  const SixWindows = window.innerHeight * 6
+  /** 
+  // SPLIDE | Slider Avis
+  const SplideAvis = new Splide('.splide-avis', {
+    type: 'loop',
+    perPage: 4,
+    perMove: 1,
+    focus: 1,
+    autoplay: true,
+  })
+  splide.mount()
 
-console.log('Welcome to Vite + JS + Webflow!')
+  // SPLIDE | Slider Finalite
+  const SplideFinalite = new Splide('.splide-finalite', {
+    type: 'loop',
+    perPage: 5,
+    perMove: 1,
+    focus: 1,
+    autoplay: true,
+  })
+  splide.mount()
+  **/
+  // PROGMARKET | Initial State de la section ProgMarket
+  $('.layout--header-large.mb-48.chap-2').removeClass('show')
+  $('.chap-obj__infos.chap-2').removeClass('show')
+  $('.chap-obj__card.chap-2').removeClass('show')
 
-createBadge()
-animateTitle()
+  $('.layout--header-large.mb-48.chap-3').removeClass('show')
+  $('.chap-obj__infos.chap-3').removeClass('show')
+  $('.chap-obj__card.chap-3').removeClass('show')
+
+  $('.layout--header-large.mb-48.chap-4').removeClass('show')
+  $('.chap-obj__infos.chap-4').removeClass('show')
+  $('.chap-obj__card.chap-4').removeClass('show')
+
+  $('.layout--header-large.mb-48.chap-5').removeClass('show')
+  $('.chap-obj__infos.chap-5').removeClass('show')
+  $('.chap-obj__card.chap-5').removeClass('show')
+
+  $('.layout--header-large.mb-48.chap-6').removeClass('show')
+  $('.chap-obj__infos.chap-6').removeClass('show')
+  $('.chap-obj__card.chap-6').removeClass('show')
+
+  // GLOBAL | Animation au Scroll
+  $(document).on('scroll', function () {
+    // PROGMARKET | Animation au Scroll
+    // PROGMARKET | Calcul de la distance de scroll séparant entre l'écran affiché et le top de ProgMarket
+    const scrollFromTopProgMarket =
+      $(window).scrollTop() -
+      $('.prog-market__scroll').offset().top +
+      window.innerHeight
+    console.log(
+      'from DIV: ' +
+        scrollFromTopProgMarket +
+        ' | from BODY: ' +
+        $(window).scrollTop()
+    )
+    // PROGMARKET | Affichage conditionnel selon la distance de scroll à l'instant T
+    if (scrollFromTopProgMarket < TwoWindows) {
+      console.log('Introduction')
+      $('.layout--header-large.mb-48.chap-2').removeClass('show')
+      $('.chap-obj__infos.chap-2').removeClass('show')
+      $('.chap-obj__card.chap-2').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-1').delay(0).addClass('show')
+      $('.chap-obj__infos.chap-1').delay(200).addClass('show')
+      $('.chap-obj__card.chap-1').delay(400).addClass('show')
+    } else if (
+      scrollFromTopProgMarket > TwoWindows &&
+      scrollFromTopProgMarket < ThreeWindows
+    ) {
+      console.log('Fondamentaux')
+      $('.layout--header-large.mb-48.chap-1').removeClass('show')
+      $('.chap-obj__infos.chap-1').removeClass('show')
+      $('.chap-obj__card.chap-1').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-3').removeClass('show')
+      $('.chap-obj__infos.chap-3').removeClass('show')
+      $('.chap-obj__card.chap-3').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-2').delay(50).addClass('show')
+      $('.chap-obj__infos.chap-2').delay(200).addClass('show')
+      $('.chap-obj__card.chap-2').delay(400).addClass('show')
+    } else if (
+      scrollFromTopProgMarket > ThreeWindows &&
+      scrollFromTopProgMarket < FourWindows
+    ) {
+      console.log('Medium')
+      $('.layout--header-large.mb-48.chap-2').removeClass('show')
+      $('.chap-obj__infos.chap-2').removeClass('show')
+      $('.chap-obj__card.chap-2').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-4').removeClass('show')
+      $('.chap-obj__infos.chap-4').removeClass('show')
+      $('.chap-obj__card.chap-4').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-3').delay(50).addClass('show')
+      $('.chap-obj__infos.chap-3').delay(200).addClass('show')
+      $('.chap-obj__card.chap-3').delay(400).addClass('show')
+    } else if (
+      scrollFromTopProgMarket > FourWindows &&
+      scrollFromTopProgMarket < FiveWindows
+    ) {
+      console.log('Avancé')
+      $('.layout--header-large.mb-48.chap-3').removeClass('show')
+      $('.chap-obj__infos.chap-3').removeClass('show')
+      $('.chap-obj__card.chap-3').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-5').removeClass('show')
+      $('.chap-obj__infos.chap-5').removeClass('show')
+      $('.chap-obj__card.chap-5').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-4').delay(50).addClass('show')
+      $('.chap-obj__infos.chap-4').delay(200).addClass('show')
+      $('.chap-obj__card.chap-4').delay(400).addClass('show')
+    } else if (
+      scrollFromTopProgMarket > FiveWindows &&
+      scrollFromTopProgMarket < SixWindows
+    ) {
+      console.log('Expert')
+      $('.layout--header-large.mb-48.chap-4').removeClass('show')
+      $('.chap-obj__infos.chap-4').removeClass('show')
+      $('.chap-obj__card.chap-4').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-6').removeClass('show')
+      $('.chap-obj__infos.chap-6').removeClass('show')
+      $('.chap-obj__card.chap-6').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-5').delay(50).addClass('show')
+      $('.chap-obj__infos.chap-5').delay(200).addClass('show')
+      $('.chap-obj__card.chap-5').delay(400).addClass('show')
+    } else {
+      console.log('Conclusion')
+      $('.layout--header-large.mb-48.chap-5').removeClass('show')
+      $('.chap-obj__infos.chap-5').removeClass('show')
+      $('.chap-obj__card.chap-5').removeClass('show')
+
+      $('.layout--header-large.mb-48.chap-6').delay(50).addClass('show')
+      $('.chap-obj__infos.chap-6').delay(200).addClass('show')
+      $('.chap-obj__card.chap-6').delay(400).addClass('show')
+    }
+  })
+})

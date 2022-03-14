58f0f0136fe00628d9e3511690b2fa3540efec0c
Théo Roland <theo@monolith.studio>
2022-03-14 16:37:48 +0100

    Test3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+
 import './features/pageProg.js'
 import './features/pageHome.js'
 
@@ -47,7 +48,7 @@ $(function () {
     )
     // PROGMARKET | Affichage conditionnel selon la distance de scroll à l'instant T
     if (scrollFromTopProgMarket < TwoWindows) {
-      console.log('Introduction v2')
+      console.log('Introduction')
       $('.layout--header-large.mb-48.chap-2').removeClass('show')
       $('.chap-obj__infos.chap-2').removeClass('show')
       $('.chap-obj__card.chap-2').removeClass('show')
